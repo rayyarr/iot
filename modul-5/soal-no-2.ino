4a33a79c5376a04c4f45a0fa65f697e8643ba90b
Rayya RR <119288775+rayyarr@users.noreply.github.com>
2023-10-19 21:48:50 +0700

    Update soal-no-2.ino

--- modul-5/soal-no-2.ino
+++ modul-5/soal-no-2.ino
@@ -1,63 +1,82 @@
-#include <WiFi.h>
+// Node 1 (Pertama)
+
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
 
-const int DHT_PIN = 15;
-#define LED_PIN 13
-DHT dht(DHT_PIN, DHT22);
-
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
-const char* mqtt_server = "test.mosquitto.org";
-
 WiFiClient espClient;
 PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-float temp = 0;
-float hum = 0;
+
+#define WIFI_SSID "Karyawan"
+#define WIFI_PASS "@Gaspol2023ok"
+#define MQTT_SERVER "broker.hivemq.com"
+#define MQTT_PORT 1883
+const char *topic = "kelompok-nanas/sensor/temp";
+
+#define DHTPIN D4
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+#define LED1 D3
+bool ledStatus = true;
+
+unsigned long _waiting = millis();
+unsigned long _now;
+int value = 0;
+char data[50];
+char data2[50];
+
+String buffData;
 
 void setup_wifi() {
-  delay(10);
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  dht.begin();
-
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  randomSeed(micros());
-
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) { 
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+//void callback(char* topic, byte* payload, unsigned int length){}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  buffData = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    buffData += (char)payload[i];
   }
-  Serial.println();
+
+  Serial.print(topic);
+  Serial.print(" ==> ");
+  Serial.println(buffData);
+
+  if (buffData == "false") {
+    digitalWrite(LED1, LOW);
+  } else {
+    digitalWrite(LED1, HIGH);
+  }
+  /*if (strcmp(buffData.c_str(), "true") == 0) {
+    digitalWrite(LED1, LOW);
+  } else if (strcmp(buffData.c_str(), "false") == 0) {
+    digitalWrite(LED1, HIGH);
+  }*/
 }
 
 void reconnect() {
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP32Client-";
+    Serial.print("Menghubungkan MQTT... ");
+    String clientId = "ESP8266-Ray-";
     clientId += String(random(0xffff), HEX);
     if (client.connect(clientId.c_str())) {
-      Serial.println("Connected");
-      client.publish("/rayyarr/p/mqtt", "Rayyarr");
-      client.subscribe("/rayyarr/p/mqtt"); 
+      Serial.println("Berhasil");
+      client.publish(topic, "Yess... saya terkoneksi");
+      client.subscribe("kelompok-nanas/act/led");
+      kirimDetik();
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -68,11 +87,14 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  dht.begin();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
   client.setCallback(callback);
+  
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, LOW);
 }
 
 void loop() {
@@ -80,25 +102,145 @@ void loop() {
     reconnect();
   }
   client.loop();
+  kirimDetik();
 
-  unsigned long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    float temp = dht.readTemperature();
-    float hum = dht.readHumidity();
+}
 
-    Serial.print("Temperature: ");
-    Serial.println(temp);
-    Serial.print("Humidity: ");
-    Serial.println(hum);
+void kirimDetik() {
+  _now = millis();
+  if (millis() - _waiting > 1000) {
+    _waiting = _now;
+    value++;
+    
+    float suhu = dht.readTemperature();
 
-    client.publish("/rayyarr/p/temp", String(temp, 2).c_str());
-    client.publish("/rayyarr/p/hum", String(hum, 1).c_str());
-
-    if (temp > 29) {
-      digitalWrite(LED_PIN, HIGH);
-    } else {
-      digitalWrite(LED_PIN, LOW);
+    if (!isnan(suhu)) {
+      Serial.print("Suhu: ");
+      Serial.println(suhu);
+      
+      char buffer[10]; // Buffer untuk mengonversi float menjadi string
+      dtostrf(suhu, 4, 2, buffer); // Mengonversi float menjadi string dengan 2 desimal
+      client.publish(topic, buffer); // Kirim suhu ke topik MQTT
     }
+  }
+}
+
+
+
+
+
+
+
+
+
+// Node 2 (Kedua)
+
+#include <WiFi.h> // ESP32
+#include <PubSubClient.h>
+#include <Arduino.h>
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+String buffData = "";
+
+#define WIFI_SSID "Karyawan"
+#define WIFI_PASS "@Gaspol2023ok"
+#define MQTT_SERVER "broker.hivemq.com"
+#define MQTT_PORT 1883
+const char *topicPub = "kelompok-nanas/act/led";
+
+#define LED1 2
+bool ledStatus = true;
+
+unsigned long _waiting = millis();
+unsigned long _now;
+int value = 0;
+char data[50];
+char data2[50];
+
+void setup_wifi() {
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topicPub, byte* payload, unsigned int length) {
+  buffData = "";
+  for (int i = 0; i < length; i++) {
+    buffData += (char)payload[i];
+  }
+
+  Serial.print(topicPub);
+  Serial.print(" ==> ");
+  Serial.println(buffData);
+
+  if (buffData.toFloat() > 29.0) {
+    //Serial.print("true");
+    ledStatus = true;
+    kirimDetik();
+  } else {
+    //Serial.print("false");
+    ledStatus = false;
+    kirimDetik();
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Menghubungkan MQTT... ");
+    String clientId = "ESP8266-Ray-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Berhasil");
+      client.publish(topicPub, "Yess... saya terkoneksi");
+      client.subscribe("kelompok-nanas/sensor/temp");
+      kirimDetik();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
+  
+  pinMode(LED1, OUTPUT);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  kirimDetik();
+}
+
+void kirimDetik() {
+  _now = millis();
+  if (millis() - _waiting > 1000) {
+    _waiting = _now;
+    sprintf(data, "true");
+    sprintf(data2, "false");
+    if (ledStatus) {
+        client.publish(topicPub, data);
+    } else {
+        client.publish(topicPub, data2);
+    }
+    
   }
 }
